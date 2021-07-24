9f3c72e05eb5ce69918f93d481a4d675b3eeaa89
Michael Harris <xero5k@yahoo.com>
2021-07-23 19:53:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(act = "roller-skate"){
+    return "This Saturday, I want to " + act + "!";
+
+}
+
+const mondayWork = function(act = "go to the office"){ 
+    return "This Monday, I will " + act + ".";
+}
+
+function wrapAdjective(parm1 = "*"){
+
+    return function(parm2 = "special"){
+        return `You are ${parm1}${parm2}${parm1}!`; 
+
+    }
+}
